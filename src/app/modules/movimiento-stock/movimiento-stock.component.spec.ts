6162d9b32651605f0fd50c0fbe77a9a221706c29
Alexander1096 <cyasno31@gmail.com>
2025-04-28 22:18:47 -0500

    Subida inicial del front-ed

---
+++ src/app/modules/movimiento-stock/movimiento-stock.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MovimientoStockComponent } from './movimiento-stock.component';
+
+describe('MovimientoStockComponent', () => {
+  let component: MovimientoStockComponent;
+  let fixture: ComponentFixture<MovimientoStockComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MovimientoStockComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(MovimientoStockComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/modules/movimiento-stock/movimiento-stock.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movimiento-stock',
+  imports: [],
+  templateUrl: './movimiento-stock.component.html',
+  styleUrl: './movimiento-stock.component.scss'
+})
+export class MovimientoStockComponent {
+
+}
